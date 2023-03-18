a771b1a7f477bd2e6012d25e1f16117a5049b2bc
Jukka Aittola <jaittola@iki.fi>
2023-03-18 09:16:08 +0200

    Modify input display colors and sizes

--- calculatrice/display/StatusRow.swift
+++ calculatrice/display/StatusRow.swift
@@ -8,6 +8,11 @@ class StatusRow: UIView {
     private let mod1Label = UILabel()
     private let mod2Label = UILabel()
 
+    private let degLabelContainer = UIView()
+    private let radLabelContainer = UIView()
+    private let mod1LabelContainer = UIView()
+    private let mod2LabelContainer = UIView()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -28,10 +33,15 @@ class StatusRow: UIView {
 
         addSubview(labelContainer)
 
-        labelContainer.addSubview(degLabel)
-        labelContainer.addSubview(radLabel)
-        labelContainer.addSubview(mod1Label)
-        labelContainer.addSubview(mod2Label)
+        [ degLabelContainer, radLabelContainer,
+          mod1LabelContainer, mod2LabelContainer].forEach { v in
+            labelContainer.addSubview(v)
+        }
+
+        degLabelContainer.addSubview(degLabel)
+        radLabelContainer.addSubview(radLabel)
+        mod1LabelContainer.addSubview(mod1Label)
+        mod2LabelContainer.addSubview(mod2Label)
 
         labelContainer.snp.makeConstraints { make in
             make.top.bottom.equalToSuperview()
@@ -40,29 +50,41 @@ class StatusRow: UIView {
             make.centerX.equalToSuperview()
         }
 
-        degLabel.snp.makeConstraints { make in
+        degLabelContainer.snp.makeConstraints { make in
             make.top.bottom.leading.equalToSuperview()
         }
-        radLabel.snp.makeConstraints { make in
-            make.top.bottom.equalTo(degLabel)
-            make.leading.equalTo(degLabel.snp.trailing).offset(12)
+        radLabelContainer.snp.makeConstraints { make in
+            make.top.bottom.equalTo(degLabelContainer)
+            make.leading.equalTo(degLabelContainer.snp.trailing).offset(12)
         }
-        mod1Label.snp.makeConstraints { make in
-            make.top.bottom.equalTo(degLabel)
-            make.leading.equalTo(radLabel.snp.trailing).offset(12)
+        mod1LabelContainer.snp.makeConstraints { make in
+            make.top.bottom.equalTo(degLabelContainer)
+            make.leading.equalTo(radLabelContainer.snp.trailing).offset(12)
         }
-        mod2Label.snp.makeConstraints { make in
-            make.top.bottom.equalTo(degLabel)
-            make.leading.equalTo(mod1Label.snp.trailing).offset(12)
+        mod2LabelContainer.snp.makeConstraints { make in
+            make.top.bottom.equalTo(degLabelContainer)
+            make.leading.equalTo(mod1LabelContainer.snp.trailing).offset(12)
             make.trailing.equalToSuperview()
+        }
+
+        [degLabel, radLabel, mod1Label, mod2Label].forEach { label in
+            label.snp.makeConstraints { make in
+                make.verticalEdges.equalToSuperview().inset(1)
+                make.horizontalEdges.equalToSuperview().inset(2)
+            }
         }
     }
 
     func setMode(_ calculatorMode: CalculatorMode) {
         degLabel.textColor = calculatorMode.angle == .Deg ? Styles.activeLabelColor : Styles.inactiveLabelColor
         radLabel.textColor = calculatorMode.angle == .Rad ? Styles.activeLabelColor : Styles.inactiveLabelColor
-        mod1Label.textColor = calculatorMode.keypadMode == .Mod1 ? Styles.activeLabelColor : Styles.inactiveLabelColor
-        mod2Label.textColor = calculatorMode.keypadMode == .Mod2 ? Styles.activeLabelColor : Styles.inactiveLabelColor
+        mod1Label.textColor = calculatorMode.keypadMode == .Mod1 ? Styles.mod1TextColor : Styles.inactiveLabelColor
+        mod2Label.textColor = calculatorMode.keypadMode == .Mod2 ? Styles.mod2TextColor : Styles.inactiveLabelColor
+
+        degLabelContainer.backgroundColor = calculatorMode.angle == .Deg ? Styles.keyBackgroundColor : .white
+        radLabelContainer.backgroundColor = calculatorMode.angle == .Rad ? Styles.keyBackgroundColor : .white
+        mod1LabelContainer.backgroundColor = calculatorMode.keypadMode == .Mod1 ? Styles.keyBackgroundColor : .white
+        mod2LabelContainer.backgroundColor = calculatorMode.keypadMode == .Mod2 ? Styles.keyBackgroundColor : .white
     }
 
     required init?(coder: NSCoder) {
--- calculatrice/utils/Styles.swift
+++ calculatrice/utils/Styles.swift
@@ -5,7 +5,7 @@ class Styles {
     static let keypadMargin: CGFloat = 2.0
 
     static let stackFont = font(.body, size: 20, weight: .semibold)
-    static let inputDisplayLabelFont = font(.body, size: 12, weight: .semibold)
+    static let inputDisplayLabelFont = font(.body, size: 14, weight: .semibold)
     static let keypadMainFont = font(.body, size: 20, weight: .semibold)
     static let keypadModFont = font(.body, size: 13, weight: .semibold)
 
@@ -15,7 +15,7 @@ class Styles {
     static let keyBackgroundColor = UIColor(hex: "2b2b40")
     static let keyPressedBackgroundColor = UIColor(hex: "7f7f7f")
 
-    static let activeLabelColor = UIColor(hex: "b30000")
+    static let activeLabelColor = UIColor.white
     static let inactiveLabelColor = UIColor(hex: "2e22ff")
 
     static let windowBackgroundColor = UIColor.black
