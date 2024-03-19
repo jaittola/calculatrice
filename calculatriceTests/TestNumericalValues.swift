6cfbb5395edf689551245f48c0bc5fea3c299488
Jukka Aittola <jaittola@iki.fi>
2024-03-19 20:34:29 +0200

    Convert calculations to use big numbers

--- calculatriceTests/TestNumericalValues.swift
+++ calculatriceTests/TestNumericalValues.swift
@@ -1,7 +1,29 @@
 import XCTest
+import BigNum
 @testable import calculatrice
 
 class TestNumericalValues: XCTestCase {
+    func testBasicWithAutoFormat() {
+        let v1 = Value(NumericalValue(BigFloat(1.2), numberFormat: .auto), id: 3)
+        XCTAssertEqual("1.2", v1.stringValue(CalculatorMode()))
+    }
+
+    func testVeryLargeNumber() {
+        // Temp, fix this when we implement formatting for large numbers
+        let v1 = Value(NumericalValue(BigFloat(sign: .plus,
+                                               exponent: 2128,
+                                               significand: 2)))
+        XCTAssertEqual(v1.stringValue(CalculatorMode()), "Inf")
+    }
+
+    func testVeryLargeNegativeNumber() {
+        // Temp, fix this when we implement formatting for large numbers
+        let v1 = Value(NumericalValue(BigFloat(sign: .minus,
+                                               exponent: 2128,
+                                               significand: 2)))
+        XCTAssertEqual(v1.stringValue(CalculatorMode()), "Inf")
+    }
+
     func testWithId() {
         let v1 = Value(NumericalValue(1.2))
         let v2 = v1.withId(4)
@@ -15,7 +37,8 @@ class TestNumericalValues: XCTestCase {
     }
 
     func testWithIdAndInputNumberFormat() {
-        let v1 = Value(NumericalValue(1.2, "My weird number format"), id: 3)
+        let v1 = Value(NumericalValue(BigFloat(1.2),
+                                      originalStringValue: "My weird number format"), id: 3)
         let v2 = v1.withId(4)
         let v1r = v1.asNum!
         let v2r = v2.asNum!
@@ -27,7 +50,7 @@ class TestNumericalValues: XCTestCase {
     }
 
     func testWithIdAndEngNumberFormat() {
-        let v1 = Value(NumericalValue(1.2, numberFormat: .eng), id: 99)
+        let v1 = Value(NumericalValue(BigFloat(1.2), numberFormat: .eng), id: 99)
         let v2 = v1.withId(4)
         let v1r = v1.asNum!
         let v2r = v2.asNum!
@@ -39,37 +62,37 @@ class TestNumericalValues: XCTestCase {
     }
 
     func testPositiveComplexNumberString() {
-        let c = ComplexValue(3, 2)
+        let c = ComplexValue(3.0, 2.0)
         XCTAssertEqual(c.stringValue(), "3 + 2i")
     }
 
     func testPositiveComplexNumberString2() {
-        let c = ComplexValue(3, 1)
+        let c = ComplexValue(3.0, 1.0)
         XCTAssertEqual(c.stringValue(), "3 + i")
     }
 
     func testComplexNumberString() {
-        let c = ComplexValue(3, -2)
+        let c = ComplexValue(3.0, -2.0)
         XCTAssertEqual(c.stringValue(), "3 - 2i")
     }
 
     func testNegativeComplexNumberString() {
-        let c = ComplexValue(-2, -9.25)
+        let c = ComplexValue(-2.0, -9.25)
         XCTAssertEqual(c.stringValue(), "-2 - 9.25i")
     }
 
     func testNegativeComplexNumberString2() {
-        let c = ComplexValue(-2, -1)
+        let c = ComplexValue(-2.0, -1.0)
         XCTAssertEqual(c.stringValue(), "-2 - i")
     }
 
     func testRealComplexNumberString() {
-        let c = ComplexValue(-2, 0)
+        let c = ComplexValue(-2.0, 0.0)
         XCTAssertEqual(c.stringValue(), "-2")
     }
 
     func testImaginaryComplexNumberString() {
-        let c = ComplexValue(0, -2.2)
+        let c = ComplexValue(0.0, -2.2)
         XCTAssertEqual(c.stringValue(), "-2.2i")
     }
 
@@ -83,66 +106,66 @@ class TestNumericalValues: XCTestCase {
     }
 
     func testRealComplexString() {
-        let c = ComplexValue(3, 0)
+        let c = ComplexValue(3.0, 0.0)
         XCTAssertEqual(c.stringValue(), "3")
     }
 
     func testComplexOneString() {
-        let c = ComplexValue(0, 1)
+        let c = ComplexValue(0.0, 1.0)
         XCTAssertEqual(c.stringValue(), "i")
     }
 
     func testComplexMinuxOneString() {
-        let c = ComplexValue(0, -1)
+        let c = ComplexValue(0.0, -1.0)
         XCTAssertEqual(c.stringValue(), "-i")
     }
 
     func testComplexString() {
-        let c = ComplexValue(0, 3.2)
+        let c = ComplexValue(0.0, 3.2)
         XCTAssertEqual(c.stringValue(), "3.2i")
     }
 
     func testComplexZeroString() {
-        let c = ComplexValue(0, 0)
+        let c = ComplexValue(0.0, 0.0)
         XCTAssertEqual(c.stringValue(), "0")
     }
 
     func testComplexCartesianToPolar() {
-        checkComplexToPolar(ComplexValue(2, 2), 2.828427, Double.pi / 4)
+        checkComplexToPolar(ComplexValue(2.0, 2.0), 2.828427, Double.pi / 4)
     }
 
     func testComplexCartesianToPolar2() {
-        checkComplexToPolar(ComplexValue(-2, 2), 2.828427, 3 * Double.pi
+        checkComplexToPolar(ComplexValue(-2.0, 2.0), 2.828427, 3 * Double.pi
          / 4)
     }
 
     func testComplexZeroToPolar() {
-        checkComplexToPolar(ComplexValue(0, 0), 0, Double.nan)
+        checkComplexToPolar(ComplexValue(0.0, 0.0), 0, Double.nan)
     }
 
     func testComplexNegToPolar() {
-        checkComplexToPolar(ComplexValue(-2, 0), 2, Double.pi)
+        checkComplexToPolar(ComplexValue(-2.0, 0.0), 2, Double.pi)
     }
 
     func testComplexPolar() {
-        checkPolarComplexToCartesian(ComplexValue(absolute: 2, argument: Double.pi / 4), sqrt(2), sqrt(2))
+        checkPolarComplexToCartesian(ComplexValue(absolute: 2, argument: BigFloat.pi / 4), sqrt(2), sqrt(2))
     }
 
     func testComplexPolar2() {
-        checkPolarComplexToCartesian(ComplexValue(absolute: 2, argument: 0), 2, 0)
+        checkPolarComplexToCartesian(ComplexValue(absolute: 2.0, argument: 0.0), 2, 0)
     }
 
     func testComplexPolar3() {
-        checkPolarComplexToCartesian(ComplexValue(absolute: 1, argument: Double.pi / 2), 0, 1)
+        checkPolarComplexToCartesian(ComplexValue(absolute: 1, argument: BigFloat.pi / 2), 0, 1)
     }
 
     func checkPolarComplexToCartesian(_ c: ComplexValue, _ real: Double, _ imaginary: Double) {
-        XCTAssertEqual(c.real.doubleValue, real, accuracy: epsilon)
-        XCTAssertEqual(c.imag.doubleValue, imaginary, accuracy: epsilon)
+        XCTAssertEqual(c.real.doubleValue, real, accuracy: NumericalValue.epsilond)
+        XCTAssertEqual(c.imag.doubleValue, imaginary, accuracy: NumericalValue.epsilond)
     }
 
     func checkComplexToPolar(_ c: ComplexValue, _ absolute: Double, _ argument: Double) {
-        XCTAssertEqual(c.polarAbsolute.doubleValue, absolute, accuracy: epsilon)
+        XCTAssertEqual(c.polarAbsolute.doubleValue, absolute, accuracy: NumericalValue.epsilond)
 
         if argument.isNaN && c.polarArgument.doubleValue.isNaN {
             return
@@ -151,7 +174,7 @@ class TestNumericalValues: XCTestCase {
             (!argument.isNaN && c.polarArgument.doubleValue.isNaN) {
             XCTFail("Arguments not equal: \(c.polarAbsolute.doubleValue) \(argument)")
         } else {
-            XCTAssertEqual(c.polarArgument.doubleValue, argument, accuracy: epsilon)
+            XCTAssertEqual(c.polarArgument.doubleValue, argument, accuracy: NumericalValue.epsilond)
         }
     }
 }
